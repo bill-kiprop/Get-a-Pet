454cfe95f77707ef7991ef1f8865f13399cff7c4
billkiprop <kipropbill3@gmail.com>
2024-06-16 00:58:22 +0300

    fetched data from server

--- frontend/src/models/petcards.jsx
+++ frontend/src/models/petcards.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { NavLink } from 'react-router-dom';
+import { BASE_URL } from './utils';
 
 function Petcard() {
+  const [pets, setPets] = useState([])
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/pets`,{
+      method:'GET',
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json()).then((data) => {
+      console.log(data)
+    }).catch((err) => console.log(err))
+  }, [])
+
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlqcmwqMCQJxUcg7jvgvC8QWYRbkxZP-za-Q&s" />
